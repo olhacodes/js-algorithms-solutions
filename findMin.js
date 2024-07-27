ffb4911f41ebb32ba52a97b4466ba9007d412da4
Naeem <developerflk@gmail.com>
2024-07-28 00:53:32 +0500

    Update findMin.js

--- findMin.js
+++ findMin.js
@@ -1,4 +1,4 @@
-// Find the minimum number in an array
+// Find the minimum number in an array 
 function findMin(arr) {
     // Use the spread operator to pass array elements as arguments to Math.min
     return Math.min(...arr);
